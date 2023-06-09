e6800d6c3c7702d615dbed7b51d64f81b996582d
msmof <m.s.morozoff@gmail.com>
2023-06-09 22:43:20 +0300

    3

--- src/test/java/utilities/PageUrls.java
+++ src/test/java/utilities/PageUrls.java
@@ -2,6 +2,6 @@ package utilities;
 
 public class PageUrls {
 
-    public static final String ACCOUNT_PAGE = "https://tms5.lightning.force.com/lightning/o/Account/new";
-    public static final String CONTACT_PAGE = "https://tms5.lightning.force.com/lightning/o/Contact/new";
+    public static final String ACCOUNT_PAGE = "https://eu44.lightning.force.com/lightning/o/Account/new";
+    public static final String CONTACT_PAGE = "https://eu44.lightning.force.com/lightning/o/Contact/new";
 }
