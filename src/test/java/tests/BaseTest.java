977caa22f34b45bd4b50751028a7d337e3eeec3e
msmof <m.s.morozoff@gmail.com>
2023-06-15 01:31:53 +0300

    8

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,6 +5,7 @@ import model.User;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Listeners;
 import steps.LoginSteps;
 import utilities.TestListener;
@@ -21,7 +22,7 @@ public class BaseTest {
         driver = DriverSingleton.getInstance().getDriver();
     }
 
-    @BeforeClass
+    @BeforeMethod
     public void loginSite() {
         LoginSteps loginSteps = new LoginSteps();
         loginSteps.login(user);
