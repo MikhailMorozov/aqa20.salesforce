ed94ba213bc2b41f83f0152ee4ab3858b8b26f03
msmof <m.s.morozoff@gmail.com>
2023-06-27 04:18:52 +0300

    14

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -10,23 +10,22 @@ import utilities.TestListener;
 
 @Listeners(TestListener.class)
 public class BaseTest {
-    protected WebDriver driver;
 
-//    User user = new User("ckotd-9hz7@force.com", "TMSAQA20onl", "https://eu44.lightning.force.com");
+    protected WebDriver driver;
     User user = new User("oybotk-zk2a@force.com", "TMSAQA20onl", "https://power-platform-6242.my.salesforce.com/");
 
-    @BeforeTest
+    @BeforeClass
     public void startBrowser() {
         driver = DriverSingleton.getInstance().getDriver();
     }
 
-    @BeforeMethod
+    @BeforeClass
     public void loginSite() {
         LoginSteps loginSteps = new LoginSteps();
         loginSteps.login(user);
     }
 
-    @AfterTest(alwaysRun = true)
+    @AfterClass(alwaysRun = true)
     public void stopBrowser() {
         DriverSingleton.getInstance().closeDriver();
     }
